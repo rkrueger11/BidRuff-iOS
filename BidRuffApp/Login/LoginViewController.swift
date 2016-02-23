35ca59d799d71838526ce375eed924b96480abf8
Ryan Krueger <rkrueger11@gmail.com>
2016-02-22 23:07:51 -0500

    Customized Login Screen

--- BidRuffApp/Login/LoginViewController.swift
+++ BidRuffApp/Login/LoginViewController.swift
@@ -7,25 +7,58 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet var nameTextField: UITextField!
     @IBOutlet var emailTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
     var viewShaker:AFViewShaker?
+    var scrollView: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        viewShaker = AFViewShaker(viewsArray: [nameTextField, emailTextField])
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+        viewShaker = AFViewShaker(viewsArray: [emailTextField, passwordTextField])
         // Do any additional setup after loading the view.
     }
-
+    
+    func keyboardWillHide(sender: NSNotification) {
+        let userInfo: [NSObject : AnyObject] = sender.userInfo!
+        let keyboardSize: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
+        self.view.frame.origin.y += keyboardSize.height
+    }
+    
+    func keyboardWillShow(sender: NSNotification) {
+        let userInfo: [NSObject : AnyObject] = sender.userInfo!
+        
+        let keyboardSize: CGSize = userInfo[UIKeyboardFrameBeginUserInfoKey]!.CGRectValue.size
+        let offset: CGSize = userInfo[UIKeyboardFrameEndUserInfoKey]!.CGRectValue.size
+        
+        if keyboardSize.height == offset.height {
+            if self.view.frame.origin.y == 0 {
+                UIView.animateWithDuration(0.1, animations: { () -> Void in
+                    self.view.frame.origin.y -= keyboardSize.height
+                })
+            }
+        } else {
+            UIView.animateWithDuration(0.1, animations: { () -> Void in
+                self.view.frame.origin.y += keyboardSize.height - offset.height
+            })
+        }
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: self.view.window)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: self.view.window)
+    }
+    
     @IBAction func loginPressed(sender: AnyObject) {
         
-        if nameTextField.text != "" && emailTextField.text != "" {
+        if emailTextField.text != "" && passwordTextField.text != ""{
             
             var user = PFUser()
-            user["fullname"] = nameTextField.text!.lowercaseString
-            user.username = emailTextField.text!.lowercaseString
-            user.password = "test"
             user.email = emailTextField.text!.lowercaseString
+            user.username = user.email
+            user.password = passwordTextField.text!
             
             user.signUpInBackgroundWithBlock {
                 (succeeded: Bool, error: NSError!) -> Void in
